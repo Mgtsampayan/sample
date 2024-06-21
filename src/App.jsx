c2e48fe689bd70dc23c875bbe9cde842d1e4c976
Gemuel <gemuel20sampayan@gmail.com>
2024-06-21 15:27:15 +0800

    Updates and Add Features

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ import './charts/ChartjsConfig';
 
 // Import pages
 import Dashboard from './pages/Dashboard';
+import ComingSoon from './components/ComingsoonVideo'
+import ForgotPassword from './components/ForgotPassword'
 import LandingPage from './components/LandingPage';
 import StudentLogin from './components/StudentLogin';
 import StudentDashboard from './pages/StudentDashboard';
+import FacultyLogin from './components/FacultyLogin';
 import FacultyDashboard from './pages/FacultyDashboard';
 import RegistrarDashboard from './pages/RegistrarDashboard';
 
@@ -30,7 +33,10 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/coming-soon" element={<ComingSoon />} />
         <Route path="/student-login" element={<StudentLogin />} />
+        <Route path="/faculty-login" element={<FacultyLogin />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/student-dashboard" element={<StudentDashboard />} />
         <Route path="/faculty-dashboard" element={<FacultyDashboard />} />
         <Route path="/registrar-dashboard" element={<RegistrarDashboard />} />
